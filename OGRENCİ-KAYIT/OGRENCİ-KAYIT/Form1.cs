bb135c596cf713d8e9283a70cb5f7976ce968073
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-12-05 14:10:39 +0300

    Add files via upload

---
+++ OGRENCİ-KAYIT/OGRENCİ-KAYIT/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OGRENCİ_KAYIT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Ogrenci ogrenci;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Ogrenci> ogrenciliste = new List<Ogrenci>();
+
+
+            ogrenciliste.Add(new Ogrenci(273,"Dilan Kaya","10A","kız","0541464927"));
+            ogrenciliste.Add(new Ogrenci(33, "Melek Erişgin", "10A", "kız", "05112131415"));
+            ogrenciliste.Add(new Ogrenci(6, "Beyza Tanyeli", "10A", "kız", "054243454647"));
+            ogrenciliste.Add(new Ogrenci(1550, "İlayda Daban", "10A", "kız", "0545464748494"));
+            ogrenciliste.Add(new Ogrenci(777, "Melisa Tuğtekin", "10A", "kız", "05636465676"));
+            ogrenciliste.Add(new Ogrenci(26, "Elif Torun", "10A", "kız", "0573747576787"));
+            ogrenciliste.Add(new Ogrenci(230, "Merve bozkurt", "10A", "kız", "05423242526"));
+            ogrenciliste.Add(new Ogrenci(245, "Sedef Şirin", "10A", "kız", "05482838485"));
+            ogrenciliste.Add(new Ogrenci(132, "Hiranur Kulakçı", "10A", "kız", "05919293949"));
+
+
+            dataGridView1.DataSource = ogrenciliste.ToList();
+
+        }
+    }
+}
---
+++ OGRENCİ-KAYIT/OGRENCİ-KAYIT/Ogrenci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGRENCİ_KAYIT
+{
+    internal class Ogrenci
+    {
+        int numara;
+        string adsoyad;
+        string sinif;
+        string cinsiyet;
+        string telefon;
+         
+
+        public int Numara { get => numara; set => numara = value; }
+        public string Adsoyad { get => adsoyad; set => adsoyad = value; }
+        public string Sinif { get => sinif; set => sinif = value; }
+        public string Cinsiyet { get => cinsiyet; set => cinsiyet = value; }
+        public string Telefon { get => telefon; set => telefon = value; }
+
+        public Ogrenci(int numara, string adsoyad, string sinif, string cinsiyet, string telefon)
+        {
+            this.numara = numara;
+            this.adsoyad = adsoyad;
+            this.sinif = sinif;
+            this.cinsiyet = cinsiyet;
+            this.telefon = telefon;
+        }
+    }
+
+
+
+}
